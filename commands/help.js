b65d323c6f6e17004e040bcc37819c14103604e7
Cephus254 <amenyacephus32@gmail.com>
2026-03-14 08:58:21 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,9 +5,9 @@ const path = require('path');
 async function helpCommand(sock, chatId, message) {
     const helpMessage = `
 ╔═══════════════════╗
-   *🤖 ${settings.botName || 'KnightBot-MD'}*  
+   *🤖 ${settings.botName || 'Cephus🫡 bot'}*  
    Version: *${settings.version || '3.0.0'}*
-   by ${settings.botOwner || 'Mr Unique Hacker'}
+   by ${settings.Cephus🫡 || 'Mr Unique Hacker'}
    YT : ${global.ytch}
 ╚═══════════════════╝
 
