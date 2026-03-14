b62c385c1033b7567299cb7cabcf8430ec2b9860
Cephus254 <amenyacephus32@gmail.com>
2026-03-14 08:50:12 +0300

    Update owner command with new contact details

--- commands/owner.js
+++ commands/owner.js
@@ -4,13 +4,13 @@ async function ownerCommand(sock, chatId) {
     const vcard = `
 BEGIN:VCARD
 VERSION:3.0
-FN:${settings.botOwner}
-TEL;waid=${settings.ownerNumber}:${settings.ownerNumber}
+FN:${settings.Cephus🫡}
+TEL;waid=${settings.254791215519}:${settings.254791215519}
 END:VCARD
 `;
 
     await sock.sendMessage(chatId, {
-        contacts: { displayName: settings.botOwner, contacts: [{ vcard }] },
+        contacts: { displayName: settings.Cephus🫡, 254791215519: [{ vcard }] },
     });
 }
 
